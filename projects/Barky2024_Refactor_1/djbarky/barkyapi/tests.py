3d4f49b7d2c3543dcd0d480c528f06603be26b40
Jeffry Babb <ahuimanu@gmail.com>
2024-03-31 11:38:49 -0500

    updated DRF

--- projects/Barky2024_Refactor_1/djbarky/barkyapi/tests.py
+++ projects/Barky2024_Refactor_1/djbarky/barkyapi/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
 
 # Create your tests here.
+# test plan
+# 1. create a bookmark
+# 2. retrieve a bookmark
+# 3. delete a bookmark
+# 4. list bookmarks
+# 5. update a bookmark
+# 6. create a snippet
+# 7. retrieve a snippet
+# 8. delete a snippet
+# 9. list snippets
+# 10. update a snippet
+# 11. create a user
+# 12. retrieve a user
+# 13. delete a user
+# 14. list users
+# 15. update a user
+# 16. highlight a snippet
+# 17. list bookmarks by user
+# 18. list snippets by user
+# 20. list bookmarks by date
+# 21. list snippets by date
+# 23. list bookmarks by title
+# 24. list snippets by title
+# 26. list bookmarks by url
+# 27. list snippets by url
+
