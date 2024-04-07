ed341572155a9d06b57de4effa882fd1b6e5d1dc
Jeffry Babb <ahuimanu@gmail.com>
2024-04-07 11:37:57 -0500

    adding tests for barky

---
+++ projects/Barky2024_Refactor_2/tests/unit/test_services.py
@@ -0,0 +1,22 @@
+# useful guide: https://www.pythontutorial.net/python-unit-testing/python-unittest/
+# note that Django's TestCase class is a subclass of Python's unittest.TestCase class so tutorials
+# on unittest.TestCase are applicable to Django's TestCase class.
+
+import unittest
+
+
+class TestCaseRepository(unittest.TestCase):
+    def test_repository_list(self):
+        pass
+
+    def test_repository_create(self):
+        pass
+
+    def test_repository_update(self):
+        pass
+
+    def test_repository_delete(self):
+        pass
+
+    def test_repository_get(self):
+        pass
